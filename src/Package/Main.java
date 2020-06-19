48d57115e626854303c4fe768fc26be8e27faead
DamianMatysko <44482004+DamianMatysko@users.noreply.github.com>
2020-06-19 23:02:48 +0200

    done

--- src/Package/Main.java
+++ src/Package/Main.java
@@ -2,6 +2,10 @@ package Package;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //new Mongo().delete();
+        new Mongo().insertOne();
+        new Mongo().insertMany();
+        new Mongo().udate();
+        new Mongo().delete();
+        new Mongo().find("Peto");
     }
 }
--- src/Package/Mongo.java
+++ src/Package/Mongo.java
@@ -1,28 +1,41 @@
 package Package;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.json.JSONObject;
+
 import java.util.concurrent.CopyOnWriteArrayList;
 
 
 public class Mongo {
-    public static MongoClient mongoClient = new MongoClient("localhost", 27017);
-    public static MongoDatabase mongoDatabase = mongoClient.getDatabase("company");
-    public static MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("users");
+    public static MongoClient mongoClient;
+    public static MongoDatabase mongoDatabase;
+    public static MongoCollection<Document> mongoCollection;
 
-    public void insertOne() throws Exception{
+    public Mongo() {
+        this.mongoClient = new MongoClient("localhost", 27017);
+        this.mongoDatabase = mongoClient.getDatabase("company");
+        this.mongoCollection = mongoDatabase.getCollection("users");
+    }
+
+    public void insertOne() throws Exception {
         Document data = new Document("name", "Peto").append("age", 12).append("status", "enchanted");
         mongoCollection.insertOne(data);
         mongoClient.close();
         System.out.println("success");
     }
 
-    public void insertMany() throws Exception{
+    public void insertMany() throws Exception {
         Document data0 = new Document("name", "Adam").append("age", 102).append("status", "out");
         Document data1 = new Document("name", "Samo").append("age", 1).append("status", "ultimate");
         Document data2 = new Document("name", "Duri").append("age", 78).append("status", "traktorista");
@@ -38,7 +51,7 @@ public class Mongo {
         System.out.println("success");
     }
 
-    public void print() throws Exception{
+    public void print() throws Exception {
         try (MongoCursor<Document> mongoCursor = mongoCollection.find().iterator()) {
             while (mongoCursor.hasNext()) {
                 System.out.println(mongoCursor.next().toJson());
@@ -46,13 +59,28 @@ public class Mongo {
         }
     }
 
-    public void udate() throws Exception{
-        mongoCollection.updateOne(Filters.eq("name", "Peto"), Updates.set("name","Petko"));
+    public void udate() throws Exception {
+        mongoCollection.updateOne(Filters.eq("name", "Peto"), Updates.set("name", "Petko"));
         System.out.println("success");
     }
 
-    public void delete() throws Exception{
+    public void delete() throws Exception {
         mongoCollection.deleteOne(Filters.eq("name", "Petko"));
         System.out.println("success");
+    }
+
+    public void find(String name) throws Exception {
+        BasicDBObject basicDBObject = new BasicDBObject();
+        basicDBObject.put("name", name);
+
+        MongoCursor<Document> mongoCursor = mongoCollection.find().iterator();
+        while (mongoCursor.hasNext()) {
+            Document doc = mongoCursor.next();
+            JSONObject object = new JSONObject(doc.toJson());
+            if (object.getString("name").equals(name)) {
+                System.out.println(object);
+                return;
+            }
+        }
     }
 }
